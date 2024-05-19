e89b19e94f56bfe75f682cf7ce382448071a0f9c
Zelskieee <chojjaarif2002@gmail.com>
2024-05-20 03:45:07 +0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for navigation links
+$(document).ready(function(){
+    $("nav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
